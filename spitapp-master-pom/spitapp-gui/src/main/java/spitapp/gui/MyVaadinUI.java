26171887a4274ddb6991a072a799523694bc2fdd
vonop1 <vonop1@bfh.ch>
2013-05-21 16:24:25 +0200

    Code conventions

--- spitapp-master-pom/spitapp-gui/src/main/java/spitapp/gui/MyVaadinUI.java
+++ spitapp-master-pom/spitapp-gui/src/main/java/spitapp/gui/MyVaadinUI.java
@@ -29,51 +29,32 @@ public class MyVaadinUI extends UI
     	content.setLocale(Locale.GERMAN);
     	 
     	// Add the topmost component.
-    	Label titletext = new Label("SpitApp Webfrontend made with Vaadin by Swen, Pascal[1], Pascal[2] and Roger");
-    	titletext.setWidth(800, Unit.PIXELS);
-    	titletext.setHeight(50, Unit.PIXELS);
-    	content.addComponent(titletext);
-    	content.setComponentAlignment(titletext, Alignment.MIDDLE_LEFT);
+    	Label titleText = new Label("SpitApp Webfrontend made with Vaadin by Swen, Pascal[1], Pascal[2] and Roger");
+    	titleText.setWidth(800, Unit.PIXELS);
+    	titleText.setHeight(50, Unit.PIXELS);
+    	content.addComponent(titleText);
+    	content.setComponentAlignment(titleText, Alignment.MIDDLE_LEFT);
     	 
     	// Add a horizontal layout for the bottom part.
     	HorizontalLayout bottom = new HorizontalLayout();
     	content.addComponent(bottom);
     	
+    	// Add the appointments
+        TerminEintragGuiHandler appointments = new TerminEintragGuiHandler();
+        appointments.setWidth(300, Unit.PIXELS);
+        appointments.setHeight(600, Unit.PIXELS);
         
-        TerminEintragGuiHandler termine = new TerminEintragGuiHandler();
-        termine.setWidth(300, Unit.PIXELS);
-        termine.setHeight(600, Unit.PIXELS);
-        
+        // Add a tabsheet with the other GUI components
         TabSheet tabsheet = new TabSheet();
         tabsheet.setHeight(600, Unit.PIXELS);
         tabsheet.setWidth(600, Unit.PIXELS);
-
         tabsheet.addTab(new DokumentGuiHandler(), "Dokumente");
         tabsheet.addTab(new SpesenGuiHandler() , "Spesen");
         tabsheet.addTab(new TaskListeGuiHandler(), "ToDo's");
         tabsheet.addTab(new ZeitAnpassungGuiHandler(), "Zeitrapporte");
        
-     
-    	bottom.addComponent(termine);
+        // Add the components to the buttom layout
+    	bottom.addComponent(appointments);
     	bottom.addComponent(tabsheet);
-    	
-    	
-//        final VerticalLayout layout = new VerticalLayout();
-//        layout.setSizeFull();
-//        layout.setMargin(true);
-//        setContent(layout);
-//        
-//        DokumentGuiHandler docs = new DokumentGuiHandler();
-//        docs.setSizeFull();
-//        //this.addWindow(window)
-//        
-////        Button button = new Button("Spitapp rules");
-////        button.addClickListener(new Button.ClickListener() {
-////            public void buttonClick(ClickEvent event) {
-////                layout.addComponent(new Label("Thank you for clicking"));
-////            }
-////        });
-//        layout.addComponent(docs);
     }
-
 }
