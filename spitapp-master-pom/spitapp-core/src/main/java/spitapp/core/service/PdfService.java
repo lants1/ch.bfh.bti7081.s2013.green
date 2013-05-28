34c21f8ad8d8ba22f6ffbac36b4c0c880dc84e06
bohnp1 <bohnp1@bfh.ch>
2013-05-28 20:57:30 +0200

    Pdf with image : test

--- spitapp-master-pom/spitapp-core/src/main/java/spitapp/core/service/PdfService.java
+++ spitapp-master-pom/spitapp-core/src/main/java/spitapp/core/service/PdfService.java
@@ -1,11 +1,15 @@
 package spitapp.core.service;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.Jpeg;
 import com.lowagie.text.pdf.PdfWriter;
 /**
  * Service used to get and create PDF-Documents
@@ -20,17 +24,21 @@ class PdfService{
     /**
      * Creates a PDF
      * @param    args    no arguments needed
+     * @throws IOException 
+     * @throws MalformedURLException 
      */
     static void main(String[] args)
-    	throws DocumentException{
+    	throws DocumentException, MalformedURLException, IOException{
     	new PdfService().createPdf("Hello World!");
     }
  
     /**
      * Creates a PDF document.
      * @throws    DocumentException
+     * @throws IOException 
+     * @throws MalformedURLException 
      */
-    byte[] createPdf(String content) throws DocumentException{
+    byte[] createPdf(String content) throws DocumentException, MalformedURLException, IOException{
         // step 1
         Document document = new Document(PageSize.A4, 50, 50, 50, 50);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -40,6 +48,7 @@ class PdfService{
         document.open();
         // step 4
         document.add(new Paragraph(content));
+        document.add(new Jpeg(getClass().getResource("/spitapp/core/images/TheOneAndOnlyLogo.jpg")));
         document.setPageCount(1);
         // step 5
         document.close();
