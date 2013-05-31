9bbdf8fb0ee479d5e6ecbd44600def121a7345c7
bohnp1 <bohnp1@bfh.ch>
2013-05-31 15:06:45 +0200

    Changed Generated PDF

--- spitapp-master-pom/spitapp-core/src/main/java/spitapp/core/service/PdfService.java
+++ spitapp-master-pom/spitapp-core/src/main/java/spitapp/core/service/PdfService.java
@@ -4,8 +4,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Jpeg;
@@ -44,8 +47,20 @@ class PdfService{
         // step 3
         document.open();
         // step 4
-        document.add(new Paragraph(content));
-        document.add(new Jpeg(getClass().getResource("/spitapp/core/images/TheOneAndOnlyLogo.jpg")));
+        Paragraph header = new Paragraph();
+        
+        Jpeg logo = new Jpeg(getClass().getResource("/spitapp/core/images/TheOneAndOnlyLogo.jpg"));
+        logo.setAlignment(Element.ALIGN_RIGHT);
+        logo.scalePercent(30); //Bildgrösse anpassen
+
+        Font font1 = new Font(Font.HELVETICA, 25, Font.BOLD); //Fett und grösser
+        Chunk title = new Chunk (content, font1);
+        
+        header.add(title);
+        header.add(logo);
+        header.setAlignment(Element.ALIGN_BOTTOM);
+        
+        document.add(header);
         document.setPageCount(1);
         // step 5
         document.close();
