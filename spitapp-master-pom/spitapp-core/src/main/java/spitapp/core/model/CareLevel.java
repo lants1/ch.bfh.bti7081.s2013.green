63966f004bd6a623431b9fb6cf0e44e894929f8f
lants1 <swen.lanthemann@gmail.com>
2013-06-04 20:39:29 +0200

    comment added

--- spitapp-master-pom/spitapp-core/src/main/java/spitapp/core/model/CareLevel.java
+++ spitapp-master-pom/spitapp-core/src/main/java/spitapp/core/model/CareLevel.java
@@ -1,3 +1,9 @@
 package spitapp.core.model;
-
+/**
+ * Represents the possible "Pflegestufen in a short enum. It ensures
+ * that only correct CareLevels will be used.
+ * 
+ * @author green
+ *
+ */
 public enum CareLevel {A1,B2}
